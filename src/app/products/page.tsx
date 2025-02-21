4852d95f49618d5c893b13ee42f831a77494f1ce
jv-taschetto <132387758+jv-taschetto@users.noreply.github.com>
2025-02-21 19:31:56 -0300

    feat add shadcn and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,7 @@
+const ProductPage = () => {
+    return <div className="p-5 border border-red-500 rounded-xl">
+            <h1 className="text-red-500">Product page</h1>
+    </div>
+}
+
+export default ProductPage;
