dffeefdf3408581c2dd7d8b5e467c54577ce884f
jv-taschetto <132387758+jv-taschetto@users.noreply.github.com>
2025-02-21 22:06:42 -0300

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
+import simpleImportSort from "eslint-plugin-simple-import-sort"; 11.2k (gzipped: 3.9k);
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins:{
+      "simple-import-sort/": simpleImportSort,
+    },
+   rules: {
+    "simple-import-sort/imports": "error",
+    "simple-export-sort/exports": "error",
+    }
+  }
 ];
 
 export default eslintConfig;
+ 
