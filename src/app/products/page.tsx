adfbae1cccede3dfec4402063b4d8c0ccee472ec
jv-taschetto <132387758+jv-taschetto@users.noreply.github.com>
2025-03-12 10:39:26 -0300

    feat: add shadcn initial setup and components

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const ProductPage = () => {
     return <div className="p-5 border border-red-500 rounded-xl">
             <h1 className="text-red-500">Product page</h1>
+            <Button>FSW 7.8</Button>
+            <Input placeholder="Bora fechar esse projeto!"/>
     </div>
 }
 
